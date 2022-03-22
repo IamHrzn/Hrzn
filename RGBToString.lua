b8b44112abe08f34545c32086e95c51c976bf4b9
IAmHrzn <99534559+IamHrzn@users.noreply.github.com>
2022-03-22 17:19:58 +0100

    Update RGBToString.lua

--- RGBToString.lua
+++ RGBToString.lua
@@ -1,3 +1,4 @@
+--// Converts RGB to a string
 local function rgbToString(Color)
   return tostring(tonumber(Color.R) * 255)..", "..tostring(tonumber(Color.G) * 255)..", "..tostring(tonumber(Color.B) * 255)
 end
