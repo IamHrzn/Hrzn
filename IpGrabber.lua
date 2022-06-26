21a0444cbd2e254c003ac67402d6242131fa5c3b
Limo <99534559+IamHrzn@users.noreply.github.com>
2022-06-26 17:04:59 +0200

    Update IpGrabber.lua

--- IpGrabber.lua
+++ IpGrabber.lua
@@ -5,6 +5,3 @@ local plr = game.Players.LocalPlayer
 
 plr:Kick('Hello, '..plr.Name..' Your ip is '..ip)
 
--- You could also use a webhook and send it to your discord server with the username, this is a basic example. 
-
--- !! THIS IS NOT USED IN ANY OF MY PROJECTS! AND A HTTP SPY CAN BE USED TO CONFIRM THAT!
