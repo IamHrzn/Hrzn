3f101dfe74f462546af25e84bc0fa29f9b7c885e
Limo <99534559+IamHrzn@users.noreply.github.com>
2023-07-11 13:58:53 +0200

    Create Values.lua

---
+++ Values.lua
@@ -0,0 +1,82 @@
+Values = {
+    GUI_Toggle_Key = Enum.KeyCode.RightControl,
+    ERRORS = {
+        [1] = "PathfindingService: path request is too long."
+    },
+    Players = {}, --> Players in the game
+    Connections = {}, --> Connections throughout the script that have been made
+    Materials = { 
+        'Plastic', 'SmoothPlastic',
+        'Neon', 'Wood',
+        'WoodPlanks', 'Marble',
+        'Slate', 'Concrete',
+        'Granite', 'Brick',
+        'Pebble', 'Cobblestone',
+        'CorrodedMetal', 'DiamondPlate',
+        'Foil', 'Metal',
+        'Grass', 'Sand',
+        'Fabric', 'Ice',
+        'Glass', 'ForceField'
+    }, --> Materials that roblox has to offer
+    KeyCodes = {
+
+        m1 = 0x01,
+        m2 = 0x02,
+        backspace = 0x08,
+        tab = 0x09,
+        clear = 0x0C,
+        enter = 0x0D,
+        shift = 0x10,
+        ctrl = 0x11,
+        alt = 0x12,
+        pause = 0x13,
+        capsLock = 0x14,
+
+        Zero = 0x30,
+        One = 0x31,
+        Two = 0x32,
+        Three = 0x33,
+        Four = 0x34,
+        Five = 0x35,
+        Six = 0x36,
+        Seven = 0x37,
+        Eight = 0x38,
+        Nine = 0x39,
+
+        A = 0x41,
+        B = 0x42,
+        C = 0x43,
+        D = 0x44,
+        E = 0x45,
+        F = 0x46,
+        G = 0x47,
+        H = 0x48,
+        I = 0x49,
+        J = 0x4A,
+        K = 0x4B,
+        L = 0x4C,
+        M = 0x4D,
+        N = 0x4E,
+        O = 0x4F,
+        P = 0x50,
+        Q = 0x51,
+        R = 0x52,
+        S = 0x53,
+        T = 0x54,
+        U = 0x55,
+        V = 0x56,
+        W = 0x57,
+        X = 0x58,
+        Y = 0x59,
+        Z = 0x5A,
+
+        Multiply = 0x6A,
+        Addition = 0x6B,
+        Seperator = 0x6C,
+        Subtraction = 0x6D,
+        Decimal = 0x6E,
+        Division = 0x6F 
+    }
+}
+
+return Values
